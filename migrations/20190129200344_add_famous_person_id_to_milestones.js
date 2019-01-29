748d27708a7b4122b8eca54b4fb3b25f826dc0ff
Arthur Lam <lam.arthur0311@gmail.com>
2019-01-29 20:19:32 +0000

    file created for testing migration rollback and migration best practice

---
+++ migrations/20190129200344_add_famous_person_id_to_milestones.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('milestones', function(table){
+      table.integer('famous_people_id').unsigned().index().references('id').inTable('famous_people');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('milestones')
+  ])
+};
