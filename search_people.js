cad9d987ec95524115b7e1467e424ba684a2ce57
Arthur Lam <lam.arthur0311@gmail.com>
2019-01-29 17:36:24 +0000

    modified for bit more code modularity

--- search_people.js
+++ search_people.js
@@ -13,12 +13,13 @@ const knex = require('knex')(settings);
 
 const name = process.argv[2];
 
-knex.from('famous_people').select("*").where('first_name', '=', name)
-    .then((rows) => {
+let query = knex.select("*").from('famous_people').where('first_name', '=', name);
+
+query.then((rows) => {
         for (row of rows) {
             console.log(`${row['id']} ${row['first_name']} ${row['last_name']} ${row['birthdate']}`);
         }
-    }).catch((err) => { console.log( err); throw err })
-    .finally(() => {
-        knex.destroy();
-    });
+     }).catch((err) => { console.log( err); throw err })
+     .finally(() => {
+         knex.destroy();
+     });
