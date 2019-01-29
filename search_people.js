625b11255beedb40c1d87bd926cde2dd66e4f222
Arthur Lam <lam.arthur0311@gmail.com>
2019-01-29 17:31:46 +0000

    Implemented same functionality with lookup_people.js using knex.js syntax

---
+++ search_people.js
@@ -0,0 +1,24 @@
+// Basic config
+const settings = {
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'development',
+    password : 'development',
+    database : 'test_db'
+  }
+};
+
+const knex = require('knex')(settings);
+
+const name = process.argv[2];
+
+knex.from('famous_people').select("*").where('first_name', '=', name)
+    .then((rows) => {
+        for (row of rows) {
+            console.log(`${row['id']} ${row['first_name']} ${row['last_name']} ${row['birthdate']}`);
+        }
+    }).catch((err) => { console.log( err); throw err })
+    .finally(() => {
+        knex.destroy();
+    });
